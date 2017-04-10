9dd7444828543b6afeb22bd747b0097d0e83a1de
Spatterjaaay <aja.pursova@gmail.com>
2017-04-10 16:53:54 -0700

    generated model and controller for Users, generated model for Votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410234421) do
+ActiveRecord::Schema.define(version: 20170410235026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "works", force: :cascade do |t|
     t.datetime "created_at", null: false
