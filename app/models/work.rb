487b694953fa077a8a9f9993bb0777922effb500
Spatterjaaay <aja.pursova@gmail.com>
2017-04-11 13:51:23 -0700

    wrote logic for reading data from csv, wrote validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,7 @@
 class Work < ApplicationRecord
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
 end
