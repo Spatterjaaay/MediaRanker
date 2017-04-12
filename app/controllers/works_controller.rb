ff9cafc6f77f735054ed70e759b6d47da2f306a5
Spatterjaaay <aja.pursova@gmail.com>
2017-04-11 22:26:45 -0700

    added actions to the works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,43 @@
 class WorksController < ApplicationController
+  def index
+    @works = Works.all
+  end
+
+  def show
+    @result_work = Work.find(params[:id])
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+
+    if @work.update(trip_params)
+      redirect_to work_path #movies/albums/books - list of all movies/albums/books
+    else
+      render "edit"
+    end
+  end
+
+  # def create
+  #   @work = Work.create work_params
+  #   if @eork.id != nil
+  #     redirect_to works_path
+  #   else
+  #     render "new"
+  #   end
+  # end
+
+  def destroy
+    Work.destroy(params[:id])
+    redirect_to works_path #movies/albums/books - list of all movies/albums/books
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :category, :description, :publication_year, :creator)
+  end
 end
