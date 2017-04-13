0ec2e8b37f53ba176ee4b8ae1e3cdb466f9f34c0
Spatterjaaay <aja.pursova@gmail.com>
2017-04-12 19:13:50 -0700

    created albums controller and wrote index and create method for albums and movies controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,9 @@
 class AlbumsController < ApplicationController
   def index
+    @books = Work.where(category: "album")
+  end
+
+  def create
+    super("album")
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,9 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Work.where(category: "movie")
+  end
+
+  def create
+    super("movie")
   end
 end
