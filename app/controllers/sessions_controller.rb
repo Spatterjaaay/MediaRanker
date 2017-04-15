9792066b6b6e34fb0b8489b39f709f3a6f780cad
Spatterjaaay <aja.pursova@gmail.com>
2017-04-15 14:41:48 -0700

    added key :user_name to sessions hash, so now I have access to username as well suring a session, added header and log in/out to the application.html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user
       # already exists
       session[:user_id] = user.id
+      session[:user_name] = user.name
       flash[:success] = "Successfully logged in as existing #{ user.name }"
       # redirect_to root_path
       redirect_to works_path
@@ -14,9 +15,9 @@ class SessionsController < ApplicationController
       # create new user
       user = User.new
       user.name = params[:name]
-      user.join_date = Time.now
       user.save
       session[:user_id] = user.id
+      session[:user_name] = user.name
       flash[:success] = "Successfully created new user #{ user.name } with ID #{ user.id }"
       redirect_to works_path
     end
