bed1841ab942d554be85ce121c35cda4bbb6631b
Spatterjaaay <aja.pursova@gmail.com>
2017-04-14 23:01:19 -0700

    added tests for albums controller

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe AlbumsController do
-  # it "should get index" do
-  #   get albums_index_url
-  #   value(response).must_be :success?
-  # end
+  it "should get index" do
+    get albums_path
+    must_respond_with :success
+  end
 
+  it "should create a new albums" do
+    proc {
+      post albums_path, params: { work:
+        { title: "Poodle Hat",
+          creator: "Weird Al",
+          description: "Silly songs"
+        } }
+    }.must_change 'Work.count', 1
+
+  end
+
+  it "should redirect to list after adding an album" do
+    post albums_path, params: { work:
+      { title: "Running with Scissors",
+        creator: "Weird Al",
+        description: "More sily songs"
+      } }
+    must_respond_with :redirect
+    must_redirect_to albums_path
+  end
+
+  it "should throw an error if attempting to create an album with no title" do
+    post albums_path, params: { work:
+      { title: "",
+        creator: "Rage Against the Machine",
+        description: "Alternative"
+      } }
+    assert_equal "A problem occurred: Could not create album", flash[:error]
+  end
 end
